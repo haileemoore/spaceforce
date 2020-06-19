70453bc17eeb12d89bf054b4a504fb11335670f6
Hailee Doyle <haileedoyle@Hailees-MacBook-Pro.local>
2020-06-19 14:58:59 -0500

    linked to user_model.js

--- controllers/session.js
+++ controllers/session.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/users.js');
+const User = require('../models/users_model.js');
 const bcrypt = require('bcrypt');
 
 router.post('/', (req, res) => {
