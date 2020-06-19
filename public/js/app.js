2cf6c7eb69083dfbf475ffb98205d9c8379c630d
Alex Wander <alexcwander@gmail.com>
2020-06-19 13:49:06 -0500

    updated all functions to arrow functions

--- public/js/app.js
+++ public/js/app.js
@@ -6,86 +6,93 @@ app.controller("BaseControl", [
   function ($http) {
     // Global Variables
     this.newFlight = {};
-    const controller = this
+    this.updateFlight = {};
 
-    //======
-    // editFlight
-    //======
-    this.editFlight = (flight) => {
-      $http({
-        method: "PUT",
-        url: "/flights/" + flight._id,
-        data: {
-          flightNumber: this.updatedNumber,
-          departureTime: this.updatedTime,
-          destination: this.updatedDestination,
-          price: this.updatedPrice,
-        },
-      }).then(
-        function () {
-          this.getFlights();
-        },
-        function (error) {
-          console.log(error);
-        }
-      );
-    };
+    ///////////////////////
+    // CRUD FOR FLIGHTS
+    ///////////////////////
 
-    //======
-    // deleteFlight
-    //======
-    this.deleteFlight = (flight) => {
-      $http({
-        method: "DELETE",
-        url: "/flights/" + flight._id,
-      }).then(
-        function (response) {
-          this.getFlights();
-        },
-        function (error) {
-          console.log(error);
-        }
-      );
-    };
-
-    //======
-    // getFlights
-    //======
-    this.getFlights = () => {
-      $http({
-        method: "GET",
-        url: "/flights",
-      }).then(
-        function (response) {
-          controller.flights = response.data;
-          console.log(controller.flights);
-        },
-        function (error) {
-          console.log(error);
-        }
-      );
-    };
-
-    //======
-    // createFlight
-    //======
+    // Create
     this.createFlight = () => {
       $http({
         method: "POST",
         url: "/flights",
         data: this.newFlight,
       }).then(
-        function (response) {
+        (response) => {
           this.newFlight = {};
           this.getFlights();
         },
-        function (error) {
+        (error) => {
           console.log(error);
         }
       );
     };
 
+    // Read
+    this.getFlights = () => {
+      $http({
+        method: "GET",
+        url: "/flights",
+      }).then(
+        (response) => {
+          this.flights = response.data;
+          console.log(this.flights);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    };
+
+    // Update
+    this.editFlight = (flight) => {
+      $http({
+        method: "PUT",
+        url: "/flights/" + flight._id,
+        data: this.updateFlight,
+      }).then(
+        () => {
+          this.getFlights();
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    };
+
+    // Delete
+    this.deleteFlight = (flight) => {
+      $http({
+        method: "DELETE",
+        url: "/flights/" + flight._id,
+      }).then(
+        (response) => {
+          this.getFlights();
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    };
+
+    ///////////////////////
+    // AUTHENTICATION (Sign Up, Log In, Log Out)
+    ///////////////////////
+
+    // Sign Up
+    this.signup = () => {};
+
+    // Log In
+    this.login = () => {};
+
+    // Log Out
+    this.logout = () => {};
+
+    ///////////////////////
     // On Page Load
+    ///////////////////////
+
     this.getFlights();
 
     // End of BaseControl
