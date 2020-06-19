b288a7a30f487f1e41654a3be39250dab3892517
Alex Wander <alexcwander@gmail.com>
2020-06-19 09:51:13 -0500

    added module.exports

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const Flights = require("../models/flight_model");
+const Flights = require("../models/flight_model.js");
 
 router.get("/", (req, res) => {
   res.send("GET request to the homepage");
 });
+
+module.exports = router;
