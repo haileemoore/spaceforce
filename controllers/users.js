9ac961276664eac67f07e935daf4bf2a3622f946
Alex Wander <alexcwander@gmail.com>
2020-06-20 09:18:46 -0500

    fixed code indentation

--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/users_model.js');
-const bcrypt = require('bcrypt')
+const User = require("../models/users_model.js");
+const bcrypt = require("bcrypt");
 
-router.post('/', (req, res) => {
-    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
-    User.create(req.body, (error, createdUser) => {
-        req.session.user = createdUser;
-        res.json(createdUser)
-    })
+router.post("/", (req, res) => {
+  req.body.password = bcrypt.hashSync(
+    req.body.password,
+    bcrypt.genSaltSync(10)
+  );
+  User.create(req.body, (error, createdUser) => {
+    req.session.user = createdUser;
+    res.json(createdUser);
+  });
 });
 
 module.exports = router;
--- models/users_model.js
+++ models/users_model.js
@@ -1,12 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema(
-    {
-        username: String,
-        password: String
-    }
-);
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
